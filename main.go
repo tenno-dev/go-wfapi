bae6f400f042c38710cbbac3df3f497741be86c6
Bitti09 <Bitti09@users.noreply.github.com>
2019-02-13 23:43:27 +0100

    more steps to multilang response. (And mor e learning of go :) )

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"runtime"
 	"time"
 
 	"github.com/Anderson-Lu/gofasion/gofasion"
@@ -13,15 +15,20 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-var missiontypelang = []byte(`{}`)
-var factionslang = []byte(`{}`)
-var locationlang = []byte(`{}`)
-var apidata = []byte(`{}`)
-var encjson = []byte(`{}`)
+//current supported lang
+var langid = map[string]int{
+	"en": 0,
+	"de": 1,
+}
+var missiontypelang = make([]byte, 2)
+var factionslang = make([][]byte, 2)
+var locationlang = make([][]byte, 2)
+var apidata = make([]byte, 2)
+var encjson = make([][]byte, 2)
 var json = jsoniter.ConfigCompatibleWithStandardLibrary
 
 func loadapidata() {
-	// Missiontypess
+	// WF API Source
 	url := "http://content.warframe.com/pc/dynamic/worldState.php"
 	wfClient := http.Client{
 		Timeout: time.Second * 20, // Maximum of 2 secs
@@ -45,7 +52,7 @@ func loadapidata() {
 	apidata = body
 }
 func loadlangs() {
-	// Missiontypes
+	// Missiontypes EN
 	url := "https://raw.githubusercontent.com/WFCD/warframe-worldstate-data/master/data/missionTypes.json"
 	wfClient := http.Client{
 		Timeout: time.Second * 20, // Maximum of 2 secs
@@ -68,7 +75,7 @@ func loadlangs() {
 	_, _ = io.Copy(ioutil.Discard, res.Body)
 
 	missiontypelang = body
-	// Factions
+	// Factions EN
 	url = "https://raw.githubusercontent.com/WFCD/warframe-worldstate-data/master/data/factionsData.json"
 	wfClient = http.Client{
 		Timeout: time.Second * 20, // Maximum of 2 secs
@@ -90,8 +97,9 @@ func loadlangs() {
 	}
 	_, _ = io.Copy(ioutil.Discard, res.Body)
 
-	factionslang = body
-	// Locations
+	factionslang[langid["en"]] = body
+
+	// Locations EN
 	url = "https://raw.githubusercontent.com/WFCD/warframe-worldstate-data/master/data/solNodes.json"
 	wfClient = http.Client{
 		Timeout: time.Second * 20, // Maximum of 2 secs
@@ -113,13 +121,61 @@ func loadlangs() {
 	}
 	_, _ = io.Copy(ioutil.Discard, res.Body)
 
-	locationlang = body
-}
-func translatetest(src string, langtype string) (ret string) {
-	var m map[string]interface{}
+	locationlang[langid["en"]] = body
+	// Factions DE
+	url = "https://raw.githubusercontent.com/WFCD/warframe-worldstate-data/l10n/data/de/factionsData.json"
+	wfClient = http.Client{
+		Timeout: time.Second * 20, // Maximum of 2 secs
+	}
 
-	if langtype == "faction" {
-		err := json.Unmarshal(factionslang, &m)
+	req, err = http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, getErr = wfClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+	defer res.Body.Close()
+
+	body, readErr = ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	_, _ = io.Copy(ioutil.Discard, res.Body)
+
+	factionslang[langid["de"]] = body
+
+	// Locations DE
+	url = "https://raw.githubusercontent.com/WFCD/warframe-worldstate-data/master/data/solNodes.json"
+	wfClient = http.Client{
+		Timeout: time.Second * 20, // Maximum of 2 secs
+	}
+
+	req, err = http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, getErr = wfClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+	defer res.Body.Close()
+
+	body, readErr = ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+	_, _ = io.Copy(ioutil.Discard, res.Body)
+
+	locationlang[langid["de"]] = body
+}
+func translatetest(src string, langtype string, lang string) (ret string) {
+	var m map[string]interface{}
+	langid := langid[lang]
+
+	if langtype == "faction" && lang == "en" {
+		err := json.Unmarshal(factionslang[langid], &m)
 		if err != nil {
 			panic(err)
 		}
@@ -135,7 +191,7 @@ func translatetest(src string, langtype string) (ret string) {
 		ret = string(x1)
 	}
 	if langtype == "location" {
-		err := json.Unmarshal(locationlang, &m)
+		err := json.Unmarshal(locationlang[langid], &m)
 		if err != nil {
 			panic(err)
 		}
@@ -149,16 +205,18 @@ func translatetest(src string, langtype string) (ret string) {
 func main() {
 	loadlangs()
 	loadapidata()
-	parseData()
+	encjson[1] = parseData("de")
+	encjson[0] = parseData("en")
 	gofasion.SetJsonParser(jsoniter.ConfigCompatibleWithStandardLibrary.Marshal, jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal)
 
 	router := httprouter.New()
 	router.GET("/wf", ShowData)
+	router.GET("/wf/:lang", ShowData)
 
 	log.Fatal(http.ListenAndServe(":8000", context.ClearHandler(router)))
 }
 
-func parseData() {
+func parseData(lang string) (ret []byte) {
 	type Eventmessage struct {
 		LanguageCode string
 		Message      string
@@ -237,16 +295,25 @@ func parseData() {
 		messagearray := v.Get("Messages").Array()
 		var test []Eventmessage
 		for i := range messagearray {
-			test = append(test, Eventmessage{
-				LanguageCode: messagearray[i].Get("LanguageCode").ValueStr(),
-				Message:      messagearray[i].Get("Message").ValueStr()})
+			if messagearray[i].Get("LanguageCode").ValueStr() == lang {
+				test = append(test, Eventmessage{
+					LanguageCode: messagearray[i].Get("LanguageCode").ValueStr(),
+					Message:      messagearray[i].Get("Message").ValueStr()})
+			}
+			// remove duplicate Items
+			if len(test) > 1 {
+				fmt.Println(len(test))
+				test = append(test[:1])
+			}
 		}
 		url := v.Get("Prop").ValueStr()
 		date := v.Get("Date").Get("$date").Get("$numberLong").ValueStr()
 		priority := v.Get("Priority").ValueBool()
 		mobileonly := v.Get("MobileOnly").ValueBool()
 		w := Event{ID: id, Message: test, URL: url, Date: date, priority: priority, mobileonly: mobileonly}
-		mainStruct.Events = append(mainStruct.Events, w)
+		if len(test) != 0 {
+			mainStruct.Events = append(mainStruct.Events, w)
+		}
 	}
 
 	//Goals Section
@@ -265,9 +332,9 @@ func parseData() {
 		id := v.Get("_id").Get("$oid").ValueStr()
 		started := v.Get("Activation").Get("$date").Get("$numberLong").ValueInt() / 1000
 		ended := v.Get("Expiry").Get("$date").Get("$numberLong").ValueInt() / 1000
-		missiontype := translatetest(v.Get("MissionInfo").Get("missionType").ValueStr(), "missiontype")
-		missionfaction := translatetest(v.Get("MissionInfo").Get("faction").ValueStr(), "faction")
-		missionlocation := translatetest(v.Get("MissionInfo").Get("location").ValueStr(), "location")
+		missiontype := translatetest(v.Get("MissionInfo").Get("missionType").ValueStr(), "missiontype", lang)
+		missionfaction := translatetest(v.Get("MissionInfo").Get("faction").ValueStr(), "faction", lang)
+		missionlocation := translatetest(v.Get("MissionInfo").Get("location").ValueStr(), "location", lang)
 		minEnemyLevel := v.Get("MissionInfo").Get("minEnemyLevel").ValueInt()
 		maxEnemyLevel := v.Get("MissionInfo").Get("maxEnemyLevel").ValueInt()
 		enemywaves = v.Get("MissionInfo").Get("maxWaveNum").ValueInt()
@@ -304,9 +371,9 @@ func parseData() {
 		var variants []Sortievariant
 		for i := range variantarray {
 			variants = append(variants, Sortievariant{
-				MissionType:     translatetest(variantarray[i].Get("missionType").ValueStr(), "missiontype"),
+				MissionType:     translatetest(variantarray[i].Get("missionType").ValueStr(), "missiontype", lang),
 				MissionMod:      variantarray[i].Get("modifierType").ValueStr(),
-				MissionLocation: translatetest(variantarray[i].Get("node").ValueStr(), "location"),
+				MissionLocation: translatetest(variantarray[i].Get("node").ValueStr(), "location", lang),
 				MissionTileset:  variantarray[i].Get("tileset").ValueStr(),
 			})
 		}
@@ -353,11 +420,37 @@ func parseData() {
 			mainStruct.SyndicateMissions = append(mainStruct.SyndicateMissions, w)
 		}
 	}
-	encjson, _ = json.Marshal(mainStruct)
+	ret, _ = json.Marshal(mainStruct)
+	return ret
 }
 
-// ShowData
-func ShowData(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	w.Header().Set("Content-Type", "application/json")
-	_, _ = w.Write(encjson)
+// PrintMemUsage - only fo debug
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
+
+// ShowData APIResponse
+func ShowData(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Println(p.ByName("lang"))
+	langsel := p.ByName("lang")
+	_, exists := langid[langsel]
+	fmt.Println(exists)
+	if exists == false {
+		_, _ = w.Write([]byte("unknown Langcode"))
+
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		_, _ = w.Write(encjson[langid[langsel]])
+	}
+	PrintMemUsage()
 }
