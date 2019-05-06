45f32481efefd5e1eeee8b1c8c057e359082b1dc
Bitti09 <Bitti09@users.noreply.github.com>
2019-05-06 17:26:01 +0200

    fix api fields
    forgot that we are now using warframe.com and not warframestat.us api

--- parser/syndicatemissions.go
+++ parser/syndicatemissions.go
@@ -2,73 +2,77 @@ package parser
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/bitti09/go-wfapi/datasources"
 	"github.com/buger/jsonparser"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
+
 // ParseSyndicateMissions Parse Ostrons & Solaris United Missions
 func ParseSyndicateMissions(platformno int, platform string, c mqtt.Client, lang string) {
 	type SyndicateJobs struct {
 		Jobtype        string
-		Rewards        []string
+		Rewards        string // temp until translator is added
 		MinEnemyLevel  int64
 		MaxEnemyLevel  int64
-		StandingReward []int64
+		StandingReward []string
 	}
 	type SyndicateMissions struct {
 		ID        string
 		Started   string
-		Ends      string
+		End       string
 		Syndicate string
 		Jobs      []SyndicateJobs
 	}
 	data := datasources.Apidata[platformno]
 	var syndicates []SyndicateMissions
 	jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		syndicatecheck, _ := jsonparser.GetString(value, "syndicate")
-		if syndicatecheck != "Ostrons" && syndicatecheck != "Solaris United" {
-			return
+		syndicatecheck, _ := jsonparser.GetString(value, "Tag")
+		fmt.Println(syndicatecheck)
+		if syndicatecheck == "CetusSyndicate" || syndicatecheck == "SolarisSyndicate" {
+			id, _ := jsonparser.GetString(value, "_id", "$oid")
+			started := "xxxx"
+			ended := "xxxx"
+			syndicate, _ := jsonparser.GetString(value, "Tag")
+			var jobs []SyndicateJobs
+			jsonparser.ArrayEach(value, func(value1 []byte, dataType jsonparser.ValueType, offset int, err error) {
+				jobtype, _ := jsonparser.GetString(value1, "jobType")
+				rewards, _ := jsonparser.GetString(value1, "rewards") // temp until translator is added
+				/*rewards := make([]string, 0)
+				jsonparser.ArrayEach(value1, func(reward []byte, dataType jsonparser.ValueType, offset int, err error) {
+					rewards = append(rewards, string(reward))
+
+				}, "rewardPool")*/
+				minEnemyLevel, _ := jsonparser.GetInt(value1, "minEnemyLevel")
+				maxEnemyLevel, _ := jsonparser.GetInt(value1, "maxEnemyLevel")
+				standing := make([]string, 0)
+				jsonparser.ArrayEach(value1, func(xpam []byte, dataType jsonparser.ValueType, offset int, err error) {
+					standing = append(standing, string(xpam))
+
+				}, "xpAmounts")
+				jobs = append(jobs, SyndicateJobs{
+					Jobtype:        jobtype,
+					Rewards:        rewards,
+					MinEnemyLevel:  minEnemyLevel,
+					MaxEnemyLevel:  maxEnemyLevel,
+					StandingReward: standing,
+				})
+			}, "Jobs")
+
+			w := SyndicateMissions{
+				ID:        id,
+				Started:   started,
+				End:       ended,
+				Syndicate: syndicate,
+				Jobs:      jobs}
+			syndicates = append(syndicates, w)
 		}
-		id, _ := jsonparser.GetString(value, "id")
-		started, _ := jsonparser.GetString(value, "activation")
-		ended, _ := jsonparser.GetString(value, "expiry")
-		syndicate, _ := jsonparser.GetString(value, "syndicate")
-		var jobs []SyndicateJobs
-		jsonparser.ArrayEach(value, func(value1 []byte, dataType jsonparser.ValueType, offset int, err error) {
-			jobtype, _ := jsonparser.GetString(value1, "type")
-			rewards := make([]string, 0)
-			jsonparser.ArrayEach(value1, func(reward []byte, dataType jsonparser.ValueType, offset int, err error) {
-				rewards = append(rewards, string(reward))
-
-			}, "rewardPool")
-
-			minEnemyLevel, _ := jsonparser.GetInt(value1, "enemyLevels", "[0]")
-			maxEnemyLevel, _ := jsonparser.GetInt(value1, "enemyLevels", "[1]")
-			standing1, _ := jsonparser.GetInt(value1, "standingStages", "[0]")
-			standing2, _ := jsonparser.GetInt(value1, "standingStages", "[1]")
-			standing3, _ := jsonparser.GetInt(value1, "standingStages", "[2]")
-			jobs = append(jobs, SyndicateJobs{
-				Jobtype:        jobtype,
-				Rewards:        rewards,
-				MinEnemyLevel:  minEnemyLevel,
-				MaxEnemyLevel:  maxEnemyLevel,
-				StandingReward: []int64{standing1, standing2, standing3},
-			})
-		}, "jobs")
-
-		w := SyndicateMissions{
-			ID:        id,
-			Started:   started,
-			Ends:      ended,
-			Syndicate: syndicate,
-			Jobs:      jobs}
-		syndicates = append(syndicates, w)
-	}, "syndicateMissions")
+	}, "SyndicateMissions")
 
 	topicf := "/wf/" + lang + "/" + platform + "/syndicates"
 	messageJSON, _ := json.Marshal(syndicates)
+	fmt.Println("test 1", messageJSON)
 	token := c.Publish(topicf, 0, true, messageJSON)
 	token.Wait()
-
 }
