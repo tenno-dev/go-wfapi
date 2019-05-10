c99f24fa86ccdf3d0ba4ae52ebfcd8fd544e7042
Bitti09 <Bitti09@users.noreply.github.com>
2019-05-10 21:23:00 +0200

    test output

---
+++ datasources/internal.go
@@ -0,0 +1,7 @@
+package datasources
+
+// Darvodeal translated Deals
+var Darvodeal [10][]byte
+
+// Test test lang  object
+var Test = map[string]int{"en": 0, "de": 1, "es": 2, "fr": 3, "it": 4, "ko": 5, "pl": 6, "pt": 7, "ru": 8, "zh": 9}
